5c23d5c9437d78091d704b5d9f526182f467d4a0
Edward Apostol <edward.apostol.github@gmail.com>
2017-05-01 02:01:30 -0400

    #007.1 : current bundle commit from last build

---
+++ dist/js/bundle.js
@@ -0,0 +1,92 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _DefaultAlert = require("./DefaultAlert");
+
+var _DefaultAlert2 = _interopRequireDefault(_DefaultAlert);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } /**
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                * Created by Edward_J_Apostol on 2017-05-01.
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                */
+
+
+var App = function (_Object) {
+    _inherits(App, _Object);
+
+    function App() {
+        var msg = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "Edward says Hello with Gulp, SASS, Babel and ES6!";
+
+        _classCallCheck(this, App);
+
+        var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this));
+
+        _this.message = msg;
+        var oldPopUp = new _DefaultAlert2.default(_this.message);
+        oldPopUp.display();
+        return _this;
+    }
+
+    _createClass(App, [{
+        key: "init",
+        value: function init() {}
+    }]);
+
+    return App;
+}(Object);
+
+exports.default = App;
+
+},{"./DefaultAlert":2}],2:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var DefaultAlert = function () {
+  function DefaultAlert(message) {
+    _classCallCheck(this, DefaultAlert);
+
+    this.message = message;
+  }
+
+  _createClass(DefaultAlert, [{
+    key: "display",
+    value: function display() {
+      alert(this.message);
+    }
+  }]);
+
+  return DefaultAlert;
+}();
+
+exports.default = DefaultAlert;
+
+},{}],3:[function(require,module,exports){
+"use strict";
+
+var _App = require("./App");
+
+var _App2 = _interopRequireDefault(_App);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = new _App2.default("Hello From the ES6 World using Gulp + Babel, with SASS");
+
+},{"./App":1}]},{},[3]);
